113bb504ab59aef858eb67b70d3656ca51bf0ffc
abreternitz <aliceburian@gmail.com>
2018-06-19 21:03:21 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,43 @@
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
 
-# Code your methods below
+def skip_step(foot)
+  puts "Lower #{foot} foot to the ground"
+  puts "Bounce #{foot} foot back up slightly, kicking it a few inches back"
+  puts "#{foot}, other foot, #{foot} and #{foot}"
+end
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts base_footwork
+
+
+# 1. Lower right foot to the ground
+# 2. Bounce right foot back up slightly, kicking it a few inches back
+# 3. Right, Left, Right and Right
+
+
+
+# ### Upper Body
+
+# ##### Bob the reins
+
+# 1. Holding your arms out in front of you, straight and at chest level
+# 2. Cross your right wrist over your left and hold them together
+# 3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated **eight** times.
+
+# **Lasso**
+
+# 1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.
+#   - Lift right arm up and point right elbow diagonally to the right.
+#   - Raise right forearm so that it points straight up, and **whip it in small circles** to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated **eight** times as well.
+
+
+
+# #### **Bust a move:**
+
+# - Be cool
+# - Be snazzy!
+
+
